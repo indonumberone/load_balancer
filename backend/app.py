ecb706bfe16d3e1aa79b4c30796f7e7e5ce7190c
indonumberone <01muqsith@gmail.com>
2025-06-04 22:24:04 +0700

    loadbalance with Least Connection

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,10 @@ import socket
 import time
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)  # Ini bikin /metrics otomatis
-
 hostname = socket.gethostname()
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.0', instance=hostname)
 
 @app.route("/")
 def index():
@@ -15,10 +16,15 @@ def index():
 @app.route("/loadtest")
 @metrics.counter('loadtest_requests_total', 'Number of loadtest requests')
 def loadtest():
+    time.sleep(0.1)  
     return jsonify({
         "server_hostname": hostname,
         "timestamp": time.time()
     })
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
